e2cf20d95393355bf4b6bcff02e3ae4639fee57a
Gregory <gregory.leblond.dev@gmail.com>
2024-07-08 21:08:21 +0200

    feat(database): wip

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
 #![allow(unused)]
 
-pub use self::error::{Error,Result};
-
-use std::net::SocketAddr;
 use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
 use axum::{middleware, Json, Router};
 use serde::{Deserialize, Serialize};
-use sqlx::Connection;
+use sqlx::postgres::PgRow;
+use sqlx::Row;
+use sqlx::{Connection, PgConnection};
+use std::error::Error;
+use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
-mod error;
-mod web;
-
 #[tokio::main]
 async fn main() {
-    let url: &str = "postgres://postgre:root@database:5432/rustapi";
-    let mut conn: sqlx::PgConnection = sqlx::postgres::PgConnection::connect(url).await?;
-
     let routes_all = Router::new()
         .merge(routes_hello())
-        .merge(web::routes_login::routes())
         .layer(middleware::map_response(main_response_mapper))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
@@ -37,6 +31,22 @@ async fn main() {
         .unwrap();
 }
 
+async fn database_connection() -> Result<(i32), Box<dyn Error>> {
+    let url: &str = "postgres://postgres:root@database:5432/rustapi";
+    let mut conn = sqlx::postgres::PgConnection::connect(url).await?;
+
+    let res = sqlx::query("SELECT 1 + 1 as sum")
+        .fetch_one(&mut conn)
+        .await
+        .unwrap();
+
+    let sum: i32 = res.get("sum");
+
+    println!("1 + 1 = {}", sum);
+
+    Ok(sum);
+}
+
 #[derive(Deserialize, Debug)]
 struct HelloParams {
     name: Option<String>,
@@ -48,18 +58,18 @@ struct HelloResponse {
 }
 
 fn routes_hello() -> Router {
-    return Router::new()
-    .route("/hello", get(handler_hello))
-
+    return Router::new().route("/hello", get(handler_hello));
 }
 
 async fn handler_hello(Query(params): Query<HelloParams>) -> Json<HelloResponse> {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
 
+    database_connection();
+
     let name = params.name.unwrap_or("World".to_string());
 
     let response = HelloResponse {
-        message: String::from("Hello World in JSON")
+        message: String::from("Hello World in JSON"),
     };
 
     Json(response)
